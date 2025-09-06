69a77984264a00cc736c99e948182792eef17600
cityofsmiles <cityofsmiles@gmail.com>
2025-09-06 04:44:29 +0000

    yml

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/adding-subtracting-terms-flashcards/', // must match your repo name
+  base: '/adding-subtracting-terms-flashcards/', // must match repo name
 })
